7f72bc41b4517b3376864a2caee4beee68cac326
forgeryc <forgerygfx@gmail.com>
2023-11-01 09:57:47 +0000

    slide15 done

---
+++ slide15.py
@@ -0,0 +1,12 @@
+# Fish tank vol problem:
+def vol(length, depth):
+    vol = length * depth
+    return vol
+
+#Microscopy Problem:
+def magnification(slide, actual):
+    slide = slide * 10000
+    mag = slide / actual
+slide = 4 
+actual = 0.008
+magnification(4, 0.008)
