fcb3819819060f3d1de28b613a3694a875eb6cf2
forgeryc <forgerygfx@gmail.com>
2023-11-01 10:58:54 +0000

    slide17 done

--- slide17.py
+++ slide17.py
@@ -1,1 +1,18 @@
+#Energy bill calculator Problem:
+# Outputs the raw cost of energy usage
+def rawcost(prev, curr):
+    unitsUsed = curr - prev
+    calori3 = 39.3 / 3.6
+    kWh = unitsUsed * 1.022 
+    kWh = kWh * calori3
+    return kWh * 2.84
 
+# Circle properties problem:
+# Returns the: radius, area, circumference and arc length of a circle
+def properties(diameter, arcAngle):
+    radius = diameter / 2
+    radius2 = radius ** 2
+    area = 3.14 * radius2
+    circumference = 3.14 * diameter
+    arcLength = circumference * arcAngle
+    return radius, area, circumference, arcLength
