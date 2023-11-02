49c14972639aad8e1644d78c62b9e5170215edf7
TuttiFrutti1090 <77466673+TuttiFrutti1090@users.noreply.github.com>
2023-11-02 17:07:12 +0000

    Update slide14.py to meet slide 19 criteria

--- slide14.py
+++ slide14.py
@@ -1,4 +1,6 @@
 # Prints the Required output
-print("""The digits are: 0123456789
-The characters are: abcdABCD@#!£
-The alphanumerics are: 0123456789 abcdABCD@#!£""")
+def print_output():
+  print("""The digits are: 0123456789
+  The characters are: abcdABCD@#!£
+  The alphanumerics are: 0123456789 abcdABCD@#!£""")
+print_output()
